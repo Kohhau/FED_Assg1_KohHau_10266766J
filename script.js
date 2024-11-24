ab0bd5f67b17a5b82ac95e661e451119f9dce6b3
Kohhau <koh.hau5307@gmail.com>
2024-11-25 05:51:13 +0800

    made java work, changing left and right sizes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var menuIcon = document.querySelector(".menu")
-var left = document.querySelector(".left")
-menuIcon.onclick() = function () {
-    sidebar.classList.toggle(".compressed");
-}
+var menuIcon = document.querySelector(".menu");
+var left = document.querySelector(".left");
+var right = document.querySelector(".right");
+menuIcon.onclick = function () {
+    left.classList.toggle("compressed");
+    right.classList.toggle("expand");
+};
